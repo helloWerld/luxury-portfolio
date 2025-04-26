6478c3900c83bdab3b8ef0d54a4c92fe833f2bec
Russell Trzaska <08149462+helloWerld@users.noreply.github.com>
2025-04-25 23:29:09 -0400

    added blog page

---
+++ app/blog/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Blog = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      Coming Soon.
+    </div>
+  );
+};
+
+export default Blog;
